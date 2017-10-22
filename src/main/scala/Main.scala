622517927ad2ded37673adc50f0119e64f0a0250
Marcin Lason <marcin.lason1@gmail.com>
2017-10-22 11:54:24 +0200

    Prepare CartActor template

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,24 @@
-import akka.actor.ActorSystem
+import actors.{CartActor, CheckoutActor}
+import akka.actor.{ActorSystem, Props}
+
+import scala.io.StdIn
 
 object Main extends App {
   println("This is App created for testing Cart and Checkout actors:")
 
   val actorSystem = ActorSystem("e-Sklep")
+
+  try {
+    val cartActor = actorSystem.actorOf(Props[CartActor], "cartActor")
+    val checkoutActor = actorSystem.actorOf(Props[CheckoutActor], "checkoutActor")
+
+    cartActor ! "Elo"
+    cartActor ! "Elo"
+
+    println(">>> Press ENTER to exit <<<")
+    StdIn.readLine()
+  }
+  finally {
+    actorSystem.terminate()
+  }
 }
--- src/main/scala/actors/CartActor.scala
+++ src/main/scala/actors/CartActor.scala
@@ -1,20 +1,62 @@
 package actors
 
-import akka.actor.Actor
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{Actor, Timers}
+
+import scala.concurrent.duration.FiniteDuration
 
 object CartActor {
+
   case class ItemAdded()
+
   case class ItemRemoved()
+
   case class CartTimerExpired()
+
   case class CheckoutStarted()
+
   case class CheckoutCancelled()
+
   case class CheckoutClosed()
+
 }
 
-class CartActor extends Actor {
+class CartActor extends Actor with Timers {
 
-  override def receive = {
-    case _ => println("Got something")
+  val timeToDumpTheBucket = new FiniteDuration(15, TimeUnit.SECONDS)
+
+  override def receive = empty
+
+  def empty: Receive = {
+    case _ => {
+      println("Jestem empty")
+      timers.startSingleTimer(timerAction(), "15 seconds later...", timeToDumpTheBucket)
+      context.become(full)
+    }
+  }
+
+  def nonempty: Receive = {
+    case _ => {
+      println("Jestem nonempty")
+    }
+  }
+
+  def incheckout: Receive = {
+    case _ => {
+      print("Jestem inCheckout")
+    }
+  }
+
+  def full: Receive = {
+    case _ => {
+      println("Jestem full")
+      context.become(empty)
+    }
+  }
+
+  def timerAction(): Unit = {
+    println("Timer zakonczony")
   }
 }
 
