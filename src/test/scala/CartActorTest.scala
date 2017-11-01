2a74d9cfca51269c0d97541a027f04f4fec3d3cc
Marcin Lason <marcin.lason1@gmail.com>
2017-11-01 20:03:25 +0100

    Add more tests for CartActor

--- src/test/scala/CartActorTest.scala
+++ src/test/scala/CartActorTest.scala
@@ -1,34 +1,95 @@
 import actors.CartActor
-import actors.CartActor.{Empty, NonEmpty}
-import akka.actor.ActorSystem
-import akka.testkit.{TestFSMRef, TestKitBase}
-import org.scalatest.{FlatSpec, Matchers}
-import utils.Message.{AddItem, RemoveItem}
+import actors.CartActor.{Empty, InCheckout, NonEmpty}
+import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.FSM.StateTimeout
+import akka.testkit.{ImplicitSender, TestFSMRef, TestKitBase}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import utils.Message._
 
-class CartActorTest extends FlatSpec with TestKitBase with Matchers {
-
+class CartActorTest extends TestKitBase with WordSpecLike with BeforeAndAfterAll with ImplicitSender {
 
   implicit lazy val system = ActorSystem()
-
-
-  val fsm = TestFSMRef(new CartActor)
+  var fsm = TestFSMRef(new CartActor)
   val mustBeTypedProperly: TestFSMRef[CartActor.State, Int, CartActor] = fsm
 
-
-  "Cart Actor" should "become NonEmpty after adding element." in {
-    assert(fsm.stateName == Empty)
-    fsm ! AddItem
-    assert(fsm.stateData == 1)
-    assert(fsm.stateName == NonEmpty)
+  override def afterAll(): Unit = {
+    system.terminate
   }
 
-  "Cart Actor" should "become Empty again after removing all items." in {
-    assert(fsm.stateName == Empty)
-    fsm ! AddItem
-    assert(fsm.stateName == NonEmpty)
-    fsm ! RemoveItem
-    assert(fsm.stateName == Empty)
-    assert(fsm.stateData == 0)
+  "Cart Actor" should {
+    "become NonEmpty after adding element." in {
+      assert(fsm.stateName == Empty)
+      fsm ! AddItem
+      assert(fsm.stateData == 1)
+      assert(fsm.stateName == NonEmpty)
+    }
   }
 
+  "Cart Actor" should {
+    "become Empty again after removing all items." in {
+      fsm.setState(Empty, 0)
+      assert(fsm.stateName == Empty)
+      fsm ! AddItem
+      assert(fsm.stateName == NonEmpty)
+      assert(fsm.stateData == 1)
+      fsm ! RemoveItem
+      assert(fsm.stateName == Empty)
+      assert(fsm.stateData == 0)
+    }
+  }
+
+
+  "Cart Actor" should {
+    "still be NonEmpty after removing some of items." in {
+      fsm.setState(Empty, 0)
+      assert(fsm.stateName == Empty)
+      fsm ! AddItem
+      fsm ! AddItem
+      assert(fsm.stateName == NonEmpty)
+      fsm ! RemoveItem
+      assert(fsm.stateName == NonEmpty)
+      assert(fsm.stateData == 1)
+    }
+  }
+
+  "Cart Actor" should {
+    "dump bucket after specified period of time." in {
+      fsm.setState(Empty, 0)
+      assert(fsm.stateName == Empty)
+      fsm ! AddItem
+      assert(fsm.stateName == NonEmpty)
+      assert(fsm.isStateTimerActive == true)
+      fsm ! StateTimeout
+      assert(fsm.stateName == Empty)
+      assert(fsm.stateData == 0)
+    }
+  }
+
+  "Cart Actor" should {
+    "go back with it's counter after checkout cancellation." in {
+      fsm.setState(Empty, 0)
+      assert(fsm.stateName == Empty)
+      fsm ! AddItem
+      fsm ! AddItem
+      fsm ! StartCheckOut
+      assert(fsm.stateName == InCheckout)
+      fsm ! CheckoutCancelled
+      assert(fsm.stateName == NonEmpty)
+      assert(fsm.stateData == 2)
+    }
+  }
+
+//  "Cart actor" should {
+//    "become empty after closing checkout." in {
+//      fsm.setState(Empty, 0)
+//      assert(fsm.stateName == Empty)
+//      fsm ! AddItem
+//      fsm ! AddItem
+//      fsm ! StartCheckOut
+//      assert(fsm.stateName == NonEmpty)
+//      fsm ! CheckOutClosed
+//      assert(fsm.stateName == Empty)
+//      assert(fsm.stateData == 0)
+//    }
+//  }
 }
